b35473344a00d4e5f3e0d68a35ce7a112879d6d7
rownakabdullahomi <rownakabdullahomi@gmail.com>
2025-05-17 12:00:16 +0600

    practicing table alteration

--- alter.sql
+++ alter.sql
@@ -1,1 +1,23 @@
 SELECT * FROM person2;
+
+ALTER TABLE person2
+    ADD COLUMN email VARCHAR(30) DEFAULT 'default@email.com' NOT NULL;
+
+INSERT INTO person2 VALUES(7, 'Rafiq', 33, 'test@mail.com');
+
+ALTER TABLE person2
+    DROP COLUMN email;
+
+ALTER TABLE person2
+    RENAME COLUMN age to user_age;
+
+
+ALTER TABLE person2
+    ALTER COLUMN user_name TYPE VARCHAR(50);
+
+
+ALTER TABLE person2
+    ALTER COLUMN user_age SET NOT NULL;
+
+ALTER TABLE person2
+    ALTER COLUMN user_age DROP NOT NULL;
