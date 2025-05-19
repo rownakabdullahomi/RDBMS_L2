02596d7babffdb0e1bc2febffefdb731ca876c33
rownakabdullahomi <rownakabdullahomi@gmail.com>
2025-05-19 11:34:20 +0600

    working with group by

---
+++ groupBy.sql
@@ -0,0 +1,14 @@
+SELECT * FROM students;
+
+SELECT country, count(*) FROM students
+    GROUP BY country;
+SELECT country, avg(age) FROM students
+    GROUP BY country;
+
+SELECT country, avg(age) FROM students
+    GROUP BY country
+        HAVING avg(age) > 20;
+
+SELECT extract(YEAR FROM dob) as birth_year, count(*) 
+    FROM students
+    GROUP BY birth_year;
