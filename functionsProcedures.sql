10b90a4f71be6f99b0610be264bfb79562508263
rownakabdullahomi <rownakabdullahomi@gmail.com>
2025-05-22 09:56:30 +0600

    practice procedures

--- functionsProcedures.sql
+++ functionsProcedures.sql
@@ -28,3 +28,21 @@ $$
 $$;
 
 SELECT delete_emp_by_id(29);
+
+CREATE PROCEDURE remove_emp_by_id(p_emp_id INT)
+LANGUAGE plpgsql
+AS
+$$
+    DECLARE
+    test_var INT;
+    BEGIN
+        SELECT employee_id INTO test_var FROM employees2 WHERE employee_id = p_emp_id;
+        DELETE FROM employees2 WHERE employee_id = test_var;
+
+        RAISE NOTICE 'Employee removed successfully!';
+    END
+$$;
+
+CALL remove_emp_by_id(25);
+
+SELECT * FROM employees2;
