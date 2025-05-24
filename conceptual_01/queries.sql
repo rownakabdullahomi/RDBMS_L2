76946a533a51297c2d7571ae2761bbcdf59c964c
rownakabdullahomi <rownakabdullahomi@gmail.com>
2025-05-24 13:33:15 +0600

    working with set update limit offset between etc.

--- conceptual_01/queries.sql
+++ conceptual_01/queries.sql
@@ -11,3 +11,37 @@ SELECT title, price FROM books;
 
 SELECT * FROM books WHERE in_stock = TRUE;
 SELECT * FROM books WHERE in_stock = FALSE;
+SELECT * FROM books WHERE author = 'George Orwell';
+
+
+SELECT title, LENGTH(title) FROM books;
+
+SELECT COUNT(*) FROM books;
+
+SELECT AVG(price) FROM books;
+
+SELECT MAX(price), MIN(price) FROM books;
+
+SELECT * FROM books WHERE price BETWEEN 150 AND 200;
+
+SELECT * FROM books WHERE title LIKE 'The%';
+SELECT * FROM books WHERE author LIKE '%Orwell%';
+
+SELECT * FROM books WHERE title ILIKE 'the%';
+SELECT * FROM books WHERE author ILIKE '%orwell%';
+
+SELECT * FROM books WHERE author IN ('J.K. Rowling', 'George Orwell');
+
+SELECT * FROM books LIMIT 4;
+SELECT * FROM books LIMIT 4 OFFSET 4;
+
+UPDATE books SET price = price * 1.10;
+
+
+
+
+
+
+
+
+
