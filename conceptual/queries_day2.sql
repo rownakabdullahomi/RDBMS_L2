f45749ee6f5a16ea18c08fbd01a317173bde4159
rownakabdullahomi <rownakabdullahomi@gmail.com>
2025-05-28 19:28:47 +0600

    conceptual day 2 running

--- conceptual/queries_day2.sql
+++ conceptual/queries_day2.sql
@@ -4,3 +4,16 @@ SELECT * FROM publishers;
 SELECT * FROM books;
 
 SELECT b.title, b.author, p.name AS publisher FROM books b CROSS JOIN publishers p;
+
+ALTER TABLE publishers ADD COLUMN publisher_id SERIAL;
+
+SELECT * FROM books NATURAL JOIN publishers;
+
+SELECT title, price, author from books
+WHERE price > (
+    SELECT AVG(price) FROM books
+);
+
+SELECT name FROM publishers WHERE id IN (
+    SELECT publisher_id FROM books GROUP BY publisher_id HAVING COUNT (*) > 1
+)
